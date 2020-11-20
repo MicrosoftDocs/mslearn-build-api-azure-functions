79080092d11e6a022bdf1271531e290d14d2c185
Burke Holland <burkeholland@gmail.com>
2020-11-20 13:54:56 -0600

    Adds the abiilty to get the connection string

--- DB_SETUP/CREATE_DATABASE.sh
+++ DB_SETUP/CREATE_DATABASE.sh
@@ -4,30 +4,51 @@ accountName=tailwind-traders-$RANDOM
 databaseName=tailwind
 containerName=products
 
-echo "Beginning database creation process..."
+# If the database already exists, an env var called ACCOUNT_NAME will be present
+if [ -n "$API_ACCOUNT_NAME" ]; then 
+    # get the connection string from Cosmos DB
+    echo "Getting connection string..."
+    az cosmosdb keys list -n $API_ACCOUNT_NAME \
+    -g $API_GROUP_NAME \
+    --type connection-strings \
+    --query "connectionStrings[0].connectionString" -o tsv
 
-groupName=$(az group list --query "[0].name" -o tsv)
+else
 
-echo "Creating Cosmos DB database $accountName in Resource Group $groupName..."
-echo "This can take up to 10 minutes. Feel free to continue with the Learn Module. Just make sure to keep this terminal running."
-az cosmosdb create -n $accountName -g $groupName -o none
+    # Database doesn't exist. Create it...
 
-echo "Creating 'tailwind' database in $accountName..."
-az cosmosdb sql database create -a $accountName -g $groupName -n $databaseName -o none
+    echo "Beginning database creation process..."
 
-echo "Creating 'products' collection in 'tailwind' database..."
-az cosmosdb sql container create -g $groupName -a $accountName -d $databaseName -n $containerName -p /brand/name -o none
+    groupName=$(az group list --query "[0].name" -o tsv)
 
-echo "Preparing to import data..."
+    echo "Creating Cosmos DB database $accountName in Resource Group $groupName..."
+    echo "This can take up to 10 minutes. Feel free to continue with the Learn Module. Just make sure to keep this terminal running."
+    az cosmosdb create -n $accountName -g $groupName -o none
 
-endpoint=https://$accountName.documents.azure.com:443
-key=$(az cosmosdb list-keys -g $groupName -n $accountName --query "primaryMasterKey" -o json)
+    echo "Creating 'tailwind' database in $accountName..."
+    az cosmosdb sql database create -a $accountName -g $groupName -n $databaseName -o none
 
-echo "Installing Node modules..."
+    echo "Creating 'products' collection in 'tailwind' database..."
+    az cosmosdb sql container create -g $groupName -a $accountName -d $databaseName -n $containerName -p /brand/name -o none
 
-npm i --silent
+    echo "Preparing to import data..."
 
-echo "Populating database..."
-node ./POPULATE_DATABASE.js --endpoint $endpoint --key $key --databaseName $databaseName --containerName $containerName
+    endpoint=https://$accountName.documents.azure.com:443
+    key=$(az cosmosdb list-keys -g $groupName -n $accountName --query "primaryMasterKey" -o json)
 
-echo "Finished! Your database, $accountName, is now ready."
+    echo "Installing Node modules..."
+
+    npm i --silent
+
+    echo "Populating database..."
+    node ./POPULATE_DATABASE.js --endpoint $endpoint --key $key --databaseName $databaseName --containerName $containerName
+    
+    # Store the account name and resource group as env vars - we'll use them later on in the module
+    echo "export API_ACCOUNT_NAME=$accountName" >> ~/.bashrc    
+    echo "export API_GROUP_NAME=$groupName" >> ~/.bashrc
+
+    echo "Finished! Your database, $accountName, is now ready."
+
+fi
+
+
