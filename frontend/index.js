6183de6269d070fbbee66a0253f064834c9a22c2
sebga <seb.gauthier@gmail.com>
2022-02-23 10:56:22 -0500

    updated to work with vue 3

--- frontend/index.js
+++ frontend/index.js
@@ -1,89 +1,93 @@
-(function () {
-  const API = "http://127.0.0.1:7071/api";
+;(function () {
+  const API = 'http://127.0.0.1:7071/api'
 
-  new Vue({
-    el: "#app",
-    data: {
-      showModal: false,
-      products: [],
-      brands: ["Home & Pro tools", "Drills Co", "ProSaws", "ElctroDrill"],
-      newProduct: { name: "", price: null, stockUnits: null, brand: {} },
-      toast: {
-        type: "danger",
-        message: null,
-        show: false
+  const app = Vue.createApp({
+    el: '#app',
+    data() {
+      return {
+        showModal: false,
+        products: [],
+        brands: ['Home & Pro tools', 'Drills Co', 'ProSaws', 'ElctroDrill'],
+        newProduct: { name: '', price: null, stockUnits: null, brand: {} },
+        toast: {
+          type: 'danger',
+          message: null,
+          show: false,
+        },
       }
     },
     mounted() {
-      this.getProducts();
+      this.getProducts()
     },
     methods: {
       getProducts() {
         this.products = axios
           .get(`${API}/products`)
           .then((response) => {
-            this.products = response.data;
+            this.products = response.data
           })
           .catch((err) => {
-            this.showError("Get", err.message);
-          });
+            this.showError('Get', err.message)
+          })
       },
       updateProduct(index) {
         axios
           .put(`${API}/product`, this.products[index])
           .then(() => {
-            this.showSuccess("Item updated");
+            this.showSuccess('Item updated')
           })
           .catch((err) => {
-            this.showError("Update", err.message);
-          });
+            this.showError('Update', err.message)
+          })
       },
       createProduct() {
         axios
           .post(`${API}/product`, this.newProduct)
           .then((item) => {
-            this.products.push(item.data);
-            this.showSuccess("Item created");
+            this.products.push(item.data)
+            this.showSuccess('Item created')
           })
           .catch((err) => {
-            this.showError("Create", err.message);
+            this.showError('Create', err.message)
           })
           .finally(() => {
-            this.showModal = false;
-          });
+            this.showModal = false
+          })
       },
       deleteProduct(id, brandName, index) {
         axios
           .delete(`${API}/product/${id}`, {
             data: {
               brand: {
-                name: brandName
-              }
-            }
+                name: brandName,
+              },
+            },
           })
           .then(() => {
             // use the index to remove from the products array
-            this.products.splice(index, 1);
-            this.showSuccess("Item deleted");
+            this.products.splice(index, 1)
+            this.showSuccess('Item deleted')
           })
           .catch((err) => {
-            this.showError("Delete", err.message);
-          });
+            this.showError('Delete', err.message)
+          })
       },
       showError(action, message) {
-        this.showToast(`${action} failed: ${message}`, "danger");
+        this.showToast(`${action} failed: ${message}`, 'danger')
       },
       showSuccess(message) {
-        this.showToast(message, "success");
+        this.showToast(message, 'success')
       },
       showToast(message, type) {
-        this.toast.message = message;
-        this.toast.show = true;
-        this.toast.type = type;
+        this.toast.message = message
+        this.toast.show = true
+        this.toast.type = type
         setTimeout(() => {
-          this.toast.show = false;
-        }, 3000);
-      }
-    }
-  });
-})();
+          this.toast.show = false
+        }, 3000)
+      },
+    },
+  })
+
+  app.mount('#app')
+})()
