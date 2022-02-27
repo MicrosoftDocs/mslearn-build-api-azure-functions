8954bc2ec0f09186c9fca995f0fa2f044843684e
sebga <seb.gauthier@gmail.com>
2022-02-27 08:44:15 -0500

    changes

--- api/services/productsService.ts
+++ api/services/productsService.ts
@@ -1,40 +1,36 @@
-import { CosmosClient } from "@azure/cosmos";
+import { CosmosClient } from '@azure/cosmos'
 
 // Set connection string from CONNECTION_STRING value in local.settings.json
-const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const CONNECTION_STRING = process.env.CONNECTION_STRING
 
 const productService = {
   init() {
     try {
-      this.client = new CosmosClient(CONNECTION_STRING);
-      this.database = this.client.database("tailwind");
-      this.container = this.database.container("products");
+      this.client = new CosmosClient(CONNECTION_STRING)
+      this.database = this.client.database('tailwind')
+      this.container = this.database.container('products')
     } catch (err) {
-      console.log(err.message);
+      console.log(err.message)
     }
   },
   async create(productToCreate) {
-    const { resource } = await this.container.items.create(productToCreate);
-    return resource;
+    const { resource } = await this.container.items.create(productToCreate)
+    return resource
   },
   async read(): Promise<string> {
-    const iterator = this.container.items.readAll();
-    const { resources } = await iterator.fetchAll();
-    return JSON.stringify(resources);
+    const iterator = this.container.items.readAll()
+    const { resources } = await iterator.fetchAll()
+    return JSON.stringify(resources)
   },
   async update(product) {
-    const { resource } = await this.container.item(
-      product.id,
-      product.brand.name,
-    )
-      .replace(product);
-    return resource;
+    const { resource } = await this.container.item(product.id, product.brand.name).replace(product)
+    return resource
   },
   async delete(id, brandName) {
-    const result = await this.container.item(id, brandName).delete();
+    const result = await this.container.item(id, brandName).delete()
   },
-};
+}
 
-productService.init();
+productService.init()
 
-export default productService;
+export default productService
