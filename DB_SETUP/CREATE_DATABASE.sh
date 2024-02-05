3778490f6219c3b98a87feb2455da5b842cabf97
Julia Muiruri <julia.muiruri4@gmail.com>
2024-02-05 13:40:20 +0300

    Added db and container create in create_db script

--- DB_SETUP/CREATE_DATABASE.sh
+++ DB_SETUP/CREATE_DATABASE.sh
@@ -11,3 +11,9 @@ groupName=$(az group list --query "[0].name" -o tsv)
 echo "Creating Cosmos DB database $accountName in Resource Group $groupName..."
 echo "This can take up to 10 minutes. Feel free to continue with the Learn Module."
 az cosmosdb create -n $accountName -g $groupName -o none
+
+echo "Creating database $databaseName in Cosmos DB account $accountName..."
+az cosmosdb sql database create -a $accountName -g $groupName -n $databaseName -o none
+
+echo "Creating container $containerName in database $databaseName..."
+az cosmosdb sql container create -a $accountName -g $groupName -d $databaseName -n $containerName -p "/id" -o none
