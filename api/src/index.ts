1cebd3e1baf4ad4c09249cb2cda4ae38f6d213a2
Julia Muiruri <julia.muiruri4@gmail.com>
2024-02-15 10:30:56 +0300

    Removed ref to GetProducts and changed methods to default

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,33 +1,22 @@
 import { app } from "@azure/functions";
 import { CreateProduct } from "./functions/CreateProduct";
 import { DeleteProduct } from "./functions/DeleteProduct";
-import { GetProducts } from "./functions/GetProducts";
 import { UpdateProduct } from "./functions/UpdateProduct";
 
 app.http('CreateProduct',{
-    methods: ['POST'],
-    route: 'product',
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: CreateProduct
 })
 
 app.http('DeleteProduct', {
-    methods: ['DELETE'],
-    route: 'product/{id}',
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: DeleteProduct
 });
 
-app.http('GetProducts', {
-    methods: ['GET'],
-    route: 'products',
-    authLevel: 'anonymous',
-    handler: GetProducts
-});
-
 app.http('UpdateProduct', {
-    methods: ['PUT'],
-    route: 'product',
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: UpdateProduct
 });
