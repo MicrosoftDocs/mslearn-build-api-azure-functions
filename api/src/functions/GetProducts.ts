3b7e6d146ab66eca1defa82fceda13fdb9167a92
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-02-20 20:23:15 +0000

    chore: provide a final solution for the lesson

---
+++ api/src/functions/GetProducts.ts
@@ -0,0 +1,27 @@
+import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import productsService from "../services/product.services";
+
+export async function GetProducts(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  try {
+    let products = await productsService.read();
+
+    return {
+      status: 200,
+      jsonBody: {
+        products,
+      }
+    };
+  } catch (error: unknown) {
+    const err = error as Error;
+    context.error(`Error listing product: ${err.message}`);
+
+    return {
+      status: 500,
+      jsonBody: {
+        error: "Failed to listing products",
+      },
+    };
+  }
+};
